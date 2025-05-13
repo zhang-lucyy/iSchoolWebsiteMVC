0ec8320577ceaa5f5cab56798d8e853f2b7b3a04
Lucy Zhang <lucyz18674@gmail.com>
2025-05-13 14:07:21 -0400

    Add files via upload

---
+++ wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+// when the body is ready!
+$(function () {
+    $("#testAcc").accordion();
+    $("#pepAcc").accordion({
+        collapsible: true, active: false, heightStyle: "content"
+    });
+    $("#pepTab").tabs();
+    $("#all")
+  
+})
